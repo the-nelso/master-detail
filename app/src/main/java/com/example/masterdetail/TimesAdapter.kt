3bd1881679810fc47f9562bb5aa40f6ebb6f4cc2
Nelson Meduna <nelson.meduna@pinho.com.br>
2024-11-08 20:35:23 -0300

    Fx: Adapter

--- app/src/main/java/com/example/masterdetail/TimesAdapter.kt
+++ app/src/main/java/com/example/masterdetail/TimesAdapter.kt
@@ -1,26 +1,48 @@
 package com.example.masterdetail
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.appcompat.view.menu.MenuView.ItemView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
-class TimesAdapter (private val times: List<Time>,  private val context: Context): Adapter<TimesAdapter.TimesViewHolder>(){
-    inner class TimesViewHolder(itemView: ItemView): ViewHolder(itemView){
+class TimesAdapter (private val times: List<Time>,
+                    private val context: Context,
+                    private val click : (time: Time) -> Unit
+                    ) : Adapter<TimesAdapter.TimesViewHolder>(){
+
+
+    inner class TimesViewHolder(itemView: View): ViewHolder(itemView){
         val timeImage: ImageView = itemView.findViewById(R.id.timeImage)
         val timeName: TextView = itemView.findViewById(R.id.timeName)
         val timeEstado: TextView = itemView.findViewById(R.id.timeEstado)
         val timeDivisao: TextView = itemView.findViewById(R.id.timeDivisao)
+
+        fun bind(time: Time){
+            timeImage.setImageResource(time.imagem);
+            timeName.text = time.nome;
+            timeEstado.text = time.estado;
+            timeDivisao.text = time.divisao;
+
+            itemView.setOnClickListener{it
+                click(time)
+            }
+        }
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TimesViewHolder {
         val itemView = LayoutInflater.from(context).inflate(R.layout.list_item, parent, false)
         return TimesViewHolder(itemView)
     }
-    override fun void onBindViewHolder(holder: TimesViewHolder, position: Int) {
+
+    override fun onBindViewHolder(holder: TimesViewHolder, position: Int) {
         val time = times[position]
-        holder.timeImage.setImageResource(time.imagem)
-        holder.timeName.text = time.nome
-        holder.timeEstado.text = time.estado
-        holder.timeDivisao.text = time.divisao
+        holder.bind(time);
     }
+
     override fun getItemCount(): Int {
         return times.size
     }
