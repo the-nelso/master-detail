0225d21cb1f74946ee94dbd17683643f6f5bbb31
Nelson Meduna <nelson.meduna@pinho.com.br>
2024-11-08 19:51:05 -0300

    Fx: Metodo para Criar Times

--- app/src/main/java/com/example/masterdetail/MasterActivity.kt
+++ app/src/main/java/com/example/masterdetail/MasterActivity.kt
@@ -5,8 +5,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MasterActivity : AppCompatActivity() {
+    private lateinit var recyclerViewTimes: RecyclerView;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,26 @@ class MasterActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        recyclerViewTimes = findViewById<RecyclerView>(R.id.timesRV);
+
+        // ADAPTER AQUI
+
+        recyclerViewTimes.layoutManager = LinearLayoutManager(this);
+        recyclerViewTimes.setHasFixedSize(true);
+        recyclerViewTimes.addItemDecoration(
+            DividerItemDecoration(this, RecyclerView.VERTICAL)
+        )
+    }
+
+    private fun criarTimes() : List<Time>{
+        return listOf(
+            Time("Coritiba", "Paraná", "Segunda Divisão"),
+            Time("Atlético Paranaense", "Paraná", "Primeira Divisão"),
+            Time("Guarani", "São Paulo", "Segunda Divisão"),
+            Time("Paraná Clube", "Paraná", "Quarta Divisão"),
+            Time("Londrina", "Paraná", "Terceira Divisão"),
+            Time("Flamengo", "Rio de Janeiro", "Primeira Divisão"),
+        )
     }
 }
